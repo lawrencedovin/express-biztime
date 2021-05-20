56d60f6e1094759e7286f680ac4ad16330eb0b0b
Lawrence Dovin <lawrencedovin@gmail.com>
2021-05-20 18:40:54 -0400

    Added getting route for getting specific company

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require("../db");
 const ExpressError = require("../expressError");
 
+// GET /companies
+// Returns list of companies, like {companies: [{code, name}, ...]}
 router.get("/", async (req, res, next) => {
     try {
         const results = await db.query(`SELECT * FROM companies`);
@@ -12,19 +14,26 @@ router.get("/", async (req, res, next) => {
         return next(e);
     }
 });
-// code | name | description
-// {companies: [{code, name}, ...]}
-// return res.json({companies: result.rows})
-
-// GET /companies
-// Returns list of companies, like {companies: [{code, name}, ...]}
-
-
-
-
 
 // GET /companies/[code]
 // Return obj of company: {company: {code, name, description}}
+router.get("/:code", async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const results = await db.query(
+            `SELECT * FROM companies 
+            WHERE code=$1`, 
+            [code]);
+            
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't find user with id of ${id}`, 404);
+        }
+        return res.json({companies: results.rows[0]});
+    }
+    catch(e) {
+        return next(e);
+    }
+});
 
 // If the company given cannot be found, this should return a 404 status response.
 
