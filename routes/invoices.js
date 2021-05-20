a91e7100ce0d4d6eb249fb99b69fcea4df194fde
Lawrence Dovin <lawrencedovin@gmail.com>
2021-05-20 18:46:58 -0400

    Added getting route for getting specific invoice by company code

--- routes/invoices.js
+++ routes/invoices.js
@@ -6,7 +6,25 @@ const ExpressError = require("../expressError");
 router.get("/", async (req, res, next) => {
     try {
         const results = await db.query(`SELECT * FROM invoices`);
-        return res.json({companies: results.rows});
+        return res.json({invoices: results.rows});
+    }
+    catch(e) {
+        return next(e);
+    }
+});
+
+router.get("/:comp_code", async (req, res, next) => {
+    try {
+        const { comp_code } = req.params;
+        const results = await db.query(
+            `SELECT * FROM invoices
+            WHERE comp_code=$1`,
+            [comp_code]);
+
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't find user with id of ${id}`, 404);
+        }
+        return res.json({invoices: results.rows[0]});
     }
     catch(e) {
         return next(e);
