3aa8b014eaed9c6fa08acee74891a5726c9fcadc
Lawrence Dovin <lawrencedovin@gmail.com>
2021-05-20 19:41:31 -0400

    Added route for posting invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -31,20 +31,20 @@ router.get("/:id", async (req, res, next) => {
     }
 });
 
-// router.post("/", async (req, res, next) => {
-//     try {
-//         const { comp_code, amt, paid, add_date, paid_date } = req.body;
-//         const results = await db.query(
-//             `INSERT INTO invoices (comp_code, amt, paid, add_date, paid_date)
-//             VALUES ($1, $2, $3, $4, $5)
-//             RETURNING comp_code, amt, paid, add_date, paid_date`, 
-//             [comp_code, amt, paid, add_date, paid_date]);
-//         return res.status(201).json({company: results.rows[0]});
-//     }
-//     catch(e) {
-//         return next(e);
-//     }
-// });
+router.post("/", async (req, res, next) => {
+    try {
+        const { comp_code, amt } = req.body;
+        const results = await db.query(
+            `INSERT INTO invoices (comp_code, amt)
+            VALUES ($1, $2)
+            RETURNING comp_code, amt`, 
+            [comp_code, amt]);
+        return res.status(201).json({company: results.rows[0]});
+    }
+    catch(e) {
+        return next(e);
+    }
+});
 
 
 module.exports = router;
