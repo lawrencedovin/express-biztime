242cc81758b8f6ec1ade1781c1885139d1a97762
Lawrence Dovin <lawrencedovin@gmail.com>
2021-05-24 15:26:03 -0400

    Updated industry route to list companies belonging to that industry

--- query.sql
+++ query.sql
@@ -5,3 +5,11 @@ SELECT c.name, i.industry
         LEFT JOIN industries as i
             ON ci.industry_code = i.code
     WHERE c.code = 'apple';
+
+SELECT i.code, i.industry, c.name
+    FROM industries AS i
+        LEFT JOIN companies_industries AS ci
+            ON i.code = ci.industry_code
+        LEFT JOIN companies as c
+            ON ci.comp_code = c.code
+    WHERE i.code = 'sw';
--- routes/industry.js
+++ routes/industry.js
@@ -14,28 +14,58 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+// router.get("/:code", async (req, res, next) => {
+//     try {
+//         const { code } = req.params;
+
+//         const result = await db.query(
+//             `SELECT code, industry
+//              FROM industries
+//              WHERE code = $1`,
+//           [code]
+//       );
+
+//       if (result.rows.length === 0) {
+//         throw new ExpressError(`No such industry: ${code}`, 404)
+//       }
+  
+//       return res.json({"industry": result.rows[0]});
+//     }
+
+//     catch(e) {
+//         return next(e);
+//     }
+// });
+
 router.get("/:code", async (req, res, next) => {
     try {
-        const { code } = req.params;
 
         const result = await db.query(
-            `SELECT code, industry
-             FROM industries
-             WHERE code = $1`,
-          [code]
+            `SELECT i.code, i.industry, c.name
+            FROM industries AS i
+                LEFT JOIN companies_industries AS ci
+                    ON i.code = ci.industry_code
+                LEFT JOIN companies as c
+                    ON ci.comp_code = c.code
+             WHERE i.code = $1`,
+          [req.params.code]
       );
 
       if (result.rows.length === 0) {
         throw new ExpressError(`No such industry: ${code}`, 404)
       }
-  
-      return res.json({"industry": result.rows[0]});
+
+      let { code, industry } = result.rows[0];
+      let companies = result.rows.map(c => c.name);
+
+      return res.json({ code, industry, companies });
     }
 
     catch(e) {
         return next(e);
     }
 });
+
 
 router.post("/", async (req, res, next) => {
     try {
